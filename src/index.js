2fed46c155e076332976ae70d7d29ccb95d6f5cd
Tomas Korcak <korczis@gmail.com>
2016-10-01 20:37:40 +0200

    Attempt to hack cross-origin rules

--- src/index.js
+++ src/index.js
@@ -11,7 +11,31 @@
 //         console.log(res);
 //     });
 
+export function initialize(obj) {
+    console.log('Microcrawler.initialize()', obj);
+
+    try {
+        resizeIframe(obj);
+    } catch(err) {
+        console.log(err);
+    }
+
+    try {
+        injectScript(obj);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
 export function resizeIframe(obj) {
+    console.log('Microcrawler.resizeIframe()', obj);
     obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+}
+
+export function injectScript(obj) {
+    console.log('Microcrawler.injectScript()', obj);
+
+    const html = $("#iframe").contents().find("html").html();
+    console.log(html);
 }
 
